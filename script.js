042728d999e2bb634bdb6ba7ece983a313ba38f3
João Rafael <jrojoao@gmail.com>
2026-01-29 21:09:06 -0300

    changes

--- script.js
+++ script.js
@@ -138,3 +138,46 @@ projectItems.forEach((project, index) => {
 
    
 });
+
+particlesJS('particles-js', {
+    particles: {
+        number: {
+            value: 80,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: "#8d04ec"
+        },
+        shape: {
+            type: "circle"
+        },
+        opacity: {
+            value: 0.3,
+            random: false
+        },
+        size: {
+            value: 3,
+            random: true
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#8d04ec",
+            opacity: 0.2,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false
+        }
+    },
+    retina_detect: true
+});
